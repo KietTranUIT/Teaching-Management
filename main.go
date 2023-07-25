eaacab2022930786d06292da72f461ea202a0458
Tran Kiet <kiettranuit@gmail.com>
2023-07-25 11:24:16 +0700

    feat: run a website base

--- main.go
+++ main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"html/template"
 	"log"
+	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func ConnectDatabase() *sql.DB {
+	// Set up
 	dbDriver := "mysql"
 	dbUser := "kiettran"
 	dbPassword := "Kiet@123456"
 	dbName := "teachingManagement"
 
+	// Connect to database
 	db, err := sql.Open(dbDriver, dbUser+":"+dbPassword+"@tcp(127.0.0.1:3306)/"+dbName)
 
 	if err != nil {
@@ -23,8 +28,37 @@ func ConnectDatabase() *sql.DB {
 	return db
 }
 
-func main() {
-	db := ConnectDatabase()
+// Create a template object
+var tmpl = template.Must(template.ParseFiles("index.html"))
 
+// Handle when user access website
+func Handle(w http.ResponseWriter, r *http.Request) {
+	// Response a file index.html
+	tmpl.Execute(w, nil)
+}
+
+func main() {
+	// Connect to database on localhost
+	db := ConnectDatabase()
 	log.Println("Connection to database successful")
+	fmt.Println(db)
+
+	// Create a handler to process
+	fs := http.FileServer(http.Dir("assets"))
+
+	// Create a HTTP mux and register handle funcs
+	mux := http.NewServeMux()
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	mux.HandleFunc("/", Handle)
+
+	// Set up hostname, port, mux to run Server
+	port := "8181"
+	host := "127.0.0.1"
+	server := http.Server{
+		Addr:    fmt.Sprintf("%s:%s", host, port),
+		Handler: mux,
+	}
+
+	// Listen
+	server.ListenAndServe()
 }
